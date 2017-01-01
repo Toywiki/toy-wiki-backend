b699784b0bc3ad4904ce19f3cd1914ba953f2125
wqlin <wqlin0@gmail.com>
2017-01-01 17:59:59 +0800

    添加一个Result对象

---
+++ backend/toywiki/utils.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+class Result(dict):
+    """
+    {
+        key1 : value1,
+        key2 : value2,
+        statuscode : 1,
+    }
+    """
+
+    def __init__(self):
+        super(Result, self).__init__()
+        self["statuscode"] = 1
+
+    def setOK(self):
+        self["statuscode"] = 0
+
+    def setData(self, key, value):
+        self[key] = value
