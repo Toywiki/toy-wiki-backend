6bb456dfa559d9273784455bdfa023207918faba
hjlin0515 <hjlin0515@gmail.com>
2017-01-01 16:22:26 +0800

    实现上传图片接口

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from backend import settings
+from toywiki.views import upload_img
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^uploadimage', upload_img),
+
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
