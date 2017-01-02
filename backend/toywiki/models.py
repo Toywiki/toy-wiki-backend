1e9d89110c9a9a8d4cc71ad8c54ea0de1c3431e5
wqlin <wqlin0@gmail.com>
2017-01-02 14:48:48 +0800

    customize user model

--- backend/toywiki/models.py
+++ backend/toywiki/models.py
@@ -1,15 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 
-class User(models.Model):
+
+class UserManager(BaseUserManager):
+    def _create_user(self, account, password,
+                     is_superuser, **extra_fields):
+        user = self.model(account=account, is_superuser=is_superuser, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, account, password=None, **extra_fields):
+        return self._create_user(account, password, False, **extra_fields)
+
+    def create_superuser(self, account, password, **extra_fields):
+        return self._create_user(account, password, True, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     account = models.CharField(primary_key=True, max_length=45)
     password = models.CharField(max_length=250, blank=True, null=True)
     portrait_url = models.CharField(max_length=45, blank=True, null=True)
-    is_admin = models.IntegerField(blank=True, null=True)
+    last_login = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    is_superuser = models.IntegerField(blank=True, null=True)
     num_of_wiki = models.IntegerField(blank=True, null=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'account'
+    REQUIRED_FIELDS = []
 
     class Meta:
         managed = False
         db_table = 'user'
+        swappable = 'AUTH_USER_MODEL'
+
+    def get_full_name(self):
+        return self.account
+
+    def get_short_name(self):
+        return self.account
 
 
 class Wiki(models.Model):
@@ -28,6 +59,7 @@ class Wiki(models.Model):
         managed = False
         db_table = 'wiki'
 
+
 class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
@@ -38,7 +70,6 @@ class Comment(models.Model):
     class Meta:
         managed = False
         db_table = 'comment'
-
 
 
 class WikiUser(models.Model):
